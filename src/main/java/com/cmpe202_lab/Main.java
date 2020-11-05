a08dbce45c82a87df90a80d061ce183cf75ca1e1
revanth <revanthkrishna.maddula@sjsu.edu>
2020-11-05 13:38:08 -0800

    Initial commit

---
+++ src/main/java/com/cmpe202_lab/Main.java
@@ -0,0 +1,12 @@
+package com.cmpe202_lab;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello world");
+
+	}
+
+}
